bdf41b04947726073c8f511c7b4cdc2ecf0e3cf1
mhubii <m.huber_1994@hotmail.de>
2023-04-24 11:31:08 +0100

    added docs template

---
+++ doc/source/conf.py
@@ -0,0 +1,38 @@
+import pathlib
+import subprocess
+
+# generate doxygen
+pathlib.Path("docs/doxygen/lbr_fri_ros2_stack").mkdir(parents=True) # this is the doxygen OUTPUT_DIRECTORY
+
+# convert doxygen to sphinx, source and build directory need
+# to follow https://boschglobal.github.io/doxysphinx/docs/getting_started.html#build
+subprocess.run("doxysphinx build . $READTHEDOCS_OUTPUT/html Doxyfile", shell=True)
+
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'lbr_fri_ros2_stack'
+copyright = '2023, mhubii'
+author = 'mhubii'
+release = '1.0.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
---
+++ doc/source/index.rst
@@ -0,0 +1,14 @@
+.. lbr_fri_ros2_stack documentation master file, created by
+   sphinx-quickstart on Mon Apr 24 11:26:22 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to lbr_fri_ros2_stack's documentation!
+==============================================
+
+
+
+API
+===
+.. toctree::
+   docs/doxygen/geometry_lib/html/index
