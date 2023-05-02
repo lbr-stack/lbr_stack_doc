8f709dfc5c287d5417327bbea82998cfb010822d
mhubii <m.huber_1994@hotmail.de>
2023-05-02 15:18:33 +0100

    added links

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -37,7 +37,7 @@ release = "1.0.0"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["sphinx_mdinclude", "sphinx_copybutton"]
+extensions = ["sphinx_mdinclude", "sphinx_copybutton", "sphinx.ext.autosectionlabel"]
 
 templates_path = ["_templates"]
 exclude_patterns = []
