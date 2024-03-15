67577d4497202a356f89d5bdae86f814ed598b57
mhubii <m.huber_1994@hotmail.de>
2024-03-15 16:06:45 +0000

    remove switcher

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -77,10 +77,6 @@ html_theme = "pydata_sphinx_theme"
 html_logo = "../img/logo.png"
 html_theme_options = {
     "github_url": "https://github.com/lbr-stack/lbr_fri_ros2_stack/tree/humble",
-    "switcher": {
-        "version_match": "humble",
-        "json_url": "https://raw.githubusercontent.com/lbr-stack/lbr_stack_doc/main/doc/source/switcher.json",
-    },
     "navbar_start": ["navbar-logo", "version-switcher"],
 }
 html_context = {
